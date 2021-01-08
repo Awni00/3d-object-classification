4db37189c00404677e1e4ed688cf1700d095582f
Awni00 <awnyaltabaa@gmail.com>
2021-01-08 15:26:41 -0500

    updated train.py script
    updated to now train rgbd model using new utils

--- src/train.py
+++ src/train.py
@@ -1,12 +1,13 @@
-# TODO: restructure repo
 import os
+import time
 import argparse
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 import numpy as np
 
 
-from model import build_model
+from model import build_model, AzureLogCallback
+import utils
 
 from azureml.core import Run
 
@@ -25,48 +26,80 @@ if __name__ == "__main__":
     print("===== DATA =====")
     print("DATA PATH: " + args.data_path)
 
-    IM_SIZE = (200, 200)
-    seed = np.random.randint(0, int(1e6))
-    rgb_train_path = f"{args.data_path}/train"
-    rgb_dataset_train = image_dataset_from_directory(rgb_train_path, labels='inferred', label_mode='categorical',
-                                                                    image_size=IM_SIZE, batch_size=32, shuffle=True,
-                                                                    validation_split=0.05, subset='training', seed=seed)
-    rgb_dataset_valid = image_dataset_from_directory(rgb_train_path, labels='inferred', label_mode='categorical',
-                                                                    image_size=IM_SIZE, batch_size=32, shuffle=True,
-                                                                    validation_split=0.05, subset='validation', seed=seed)
+    # LOAD DATASET
+    train_data_path = f"{args.data_path}/train"
+    test_data_path = f"{args.data_path}/test"
 
-    rgb_test_path = f"{args.data_path}/test"
-    rgb_dataset_test = image_dataset_from_directory(rgb_test_path, labels='inferred', label_mode='categorical',
-                                                                    image_size=IM_SIZE, batch_size=32, shuffle=False)
+    start_time = time.time()
+
+    print('loading rgbd train and test datasets')
+
+    rgbd_dataset = utils.create_rgbd_dataset(train_data_path)
+    test_dataset = utils.create_rgbd_dataset(test_data_path)
+
+    print(f'done! took {time.time()-start_time:.2f} seconds')
 
     print('----------------')
     print('CLASS NAMES: ')
-    print(rgb_dataset_train.class_names)
+    print(rgbd_dataset.class_names)
     print('----------------')
 
+    # define training strategy (NOTE: not used yet in this script)
+    strategy = tf.distribute.MirroredStrategy()
+    # the number of devices
+    print(f'number of devices: {strategy.num_replicas_in_sync}')
+    run.log('number of devices', strategy.num_replicas_in_sync)
+    print('gpu device name: ', tf.test.gpu_device_name())
+    run.log('gpu device name', tf.test.gpu_device_name())
 
-    # define model
-    NUM_CLASSES = len(rgb_dataset_train.class_names)
-    INPUT_SHAPE = [200, 200, 3]
-    rgb_model = build_model(INPUT_SHAPE, NUM_CLASSES)
+    num_train_examples = len(rgbd_dataset)
+    print('num train examples: ', num_train_examples)
 
-    rgb_model.summary() # print model summary
+    BUFFER_SIZE = 10000
 
-    rgb_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    BATCH_SIZE_PER_REPLICA = 64
+    BATCH_SIZE = BATCH_SIZE_PER_REPLICA * strategy.num_replicas_in_sync
 
-    # TODO: add callbacks to log into azure, save model checkpoints, early stopping
-    # TODO: dataset prefetch, cache, shuffle, batch, etc...
-    # TODO: in-model rescale
-    # TODO: check numpy range of depth
+    train_dataset = rgbd_dataset.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+    test_dataset = test_dataset.batch(BATCH_SIZE)
+
+    # build model
+    IM_SIZE = (200, 200)
+    NUM_CLASSES = len(rgbd_dataset.class_names)
+    print('num classes: ', NUM_CLASSES)
+
+    # with strategy.scope(): # TODO
+    print('building model...')
+    start_time = time.time()
+    rgbd_model = build_model(IM_SIZE, NUM_CLASSES)
+    print(f'done! took {time.time()-start_time:.2f} seconds')
+    rgbd_model.summary()  # print model summary
+
+    # compile model normally
+    loss = tf.keras.losses.CategoricalCrossentropy()
+    opt = tf.keras.optimizers.Adam()
+
+    rgbd_model.compile(loss=loss, optimizer=opt, metrics=['accuracy'])
+
+    model_filepath = 'output/model0'
+    model_checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(
+        model_filepath, monitor='loss', save_best_only=False, save_weights_only=True, mode='auto', save_freq='epoch')
+
+    azure_log_cb = AzureLogCallback(run, metrics_to_log=['loss', 'accuracy'])
+
+    # TODO: add callbacks for: early stopping, ...
+    # TODO: dataset prefetch
     # TODO: use distributed training strategy for multiple gpus
-    rgb_model.fit(rgb_dataset_train, epochs=1, steps_per_epoch=20, validation_data=rgb_dataset_valid)
+    # TODO: image augmentation
+    EPOCHS = 1
+    rgbd_model.fit(train_dataset, epochs=EPOCHS, callbacks=[
+                   model_checkpoint_cb, azure_log_cb])  # validation_data=test_dataset, #NOTE: steps_per_epoch TEMP
 
-    loss, acc = rgb_model.evaluate(rgb_dataset_test)
-    run.log('accuracy', np.float(acc))
-    run.log('loss', np.float(loss))
+    # evaluate on test dataset
+    loss, acc = rgbd_model.evaluate(test_dataset)
+    run.log('test accuracy', np.float(acc))
+    run.log('test loss', np.float(loss))
 
-    rgb_model.save_weights('outputs/rgb_model0.h5')
-
-
+    rgbd_model.save_weights('outputs/rgbd_model0.h5')
 
     print('Finished Training')
