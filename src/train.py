0aa0457fbe6821e1c0aa5db5b438b74eb3bb6a51
Awni00 <awnyaltabaa@gmail.com>
2021-02-10 18:03:19 -0500

    updated train.py for custom training loop

--- src/train.py
+++ src/train.py
@@ -1,13 +1,15 @@
 import os
 import time
 import argparse
+import logging
+
 import tensorflow as tf
 import tensorflow.keras.backend as K
 import numpy as np
 
-
 from model import build_model, AzureLogCallback
 import utils
+import train_utils
 
 from azureml.core import Run
 
@@ -29,6 +31,7 @@ if __name__ == "__main__":
         type=str,
         help='filename of model weights')
 
+
     args = parser.parse_args()
 
     print("===== INPUTS =====")
@@ -36,6 +39,11 @@ if __name__ == "__main__":
     if (args.model_weights_path is not None) and (args.model_weights_name is not None):
         print("MODEL WEIGHTS PATH: " + args.model_weights_path)
         print('MODEL WEIGHTS NAME: ' + args.model_weights_name)
+
+    # set up logging
+    logging.basicConfig(filename='outputs/train.log', level=logging.ERROR,
+                        format='%(levelname)s %(asctime)s - %(message)s')
+    logger = logging.getLogger()
 
     # LOAD DATASET
     train_data_path = f"{args.data_path}/train"
@@ -57,12 +65,12 @@ if __name__ == "__main__":
 
 
     # NOTE: TEMP checking files  in dataset object for debugging
-    print('train dataset files: ')
-    print(rgbd_dataset.file_paths)
+    # print('train dataset files: ')
+    # print(rgbd_dataset.file_paths)
 
-    print('\n\n\n')
-    print('test dataset files: ')
-    print(test_dataset.file_paths)
+    # print('\n\n\n')
+    # print('test dataset files: ')
+    # print(test_dataset.file_paths)
 
     # # define training strategy
     # strategy = tf.distribute.MirroredStrategy()
@@ -76,9 +84,9 @@ if __name__ == "__main__":
     run.log('num train examples', num_train_examples)
 
     BUFFER_SIZE = 10000
-
     # BATCH_SIZE_PER_REPLICA = 64
-    BATCH_SIZE = 512 # BATCH_SIZE_PER_REPLICA * strategy.num_replicas_in_sync
+    # BATCH_SIZE = BATCH_SIZE_PER_REPLICA * strategy.num_replicas_in_sync
+    BATCH_SIZE = 16 # NOTE: temporarily decrease batch size to test OOM problem
 
     train_dataset = rgbd_dataset.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
     test_dataset = test_dataset.batch(BATCH_SIZE)
@@ -111,11 +119,14 @@ if __name__ == "__main__":
     run.log('trainable parameters', trainable_count)
 
 
-    # compile model
+    # prep model for training
     loss = tf.keras.losses.CategoricalCrossentropy()
-    opt = tf.keras.optimizers.Adam()
+    optimizer = tf.keras.optimizers.Adam()
 
-    rgbd_model.compile(loss=loss, optimizer=opt, metrics=['accuracy'])
+    train_metric = tf.keras.metrics.CategoricalAccuracy()
+    test_metric = tf.keras.metrics.CategoricalAccuracy()
+
+    # rgbd_model.compile(loss=loss, optimizer=opt, metrics=['accuracy'])
 
     # only evaluate against test dataset before training if weights were loaded
     if weights_loaded:
@@ -125,33 +136,67 @@ if __name__ == "__main__":
         run.log('pre-training test loss', np.float(loss))
         print('done and logged!')
 
-    # define callbacks
-    model_filepath = 'outputs/model'
-    model_checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(
-        model_filepath, monitor='loss', save_best_only=False, save_weights_only=True, mode='auto', save_freq='epoch')
+    # # define callbacks
+    # model_filepath = 'outputs/model'
+    # model_checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(
+    #     model_filepath, monitor='loss', save_best_only=False, save_weights_only=True, mode='auto', save_freq='epoch')
 
-    azure_log_cb = AzureLogCallback(run, metrics_to_log=['loss', 'accuracy', 'val_loss', 'val_accuracy'])
+    # azure_log_cb = AzureLogCallback(run, metrics_to_log=['loss', 'accuracy', 'val_loss', 'val_accuracy'])
 
 
     # TODO: add callbacks for: early stopping, ...
     # TODO: use distributed training strategy for multiple gpus (seems not as cost-efficient)
     # TODO: image augmentation
+    # TODO: custom training loop to fix loading issue. try loading (no training) loop locally (does the same problem arise?)
     print('beginning training...')
-    epoch_start = 0
-    n_epochs = 4
-    rgbd_model.fit(train_dataset, # validation_data=test_dataset,
-                    epochs=epoch_start + n_epochs, initial_epoch=epoch_start,
-                    callbacks=[model_checkpoint_cb, azure_log_cb])
+    # epoch_start = 0
+    # n_epochs = 4
+    # rgbd_model.fit(train_dataset, # validation_data=test_dataset,
+    #                 epochs=epoch_start + n_epochs, initial_epoch=epoch_start,
+    #                 callbacks=[model_checkpoint_cb, azure_log_cb])
+
+    os.mkdir('outputs/checkpoints') # make checkpoints directory
+
+    # training loop
+    epochs = 3
+
+    for epoch in range(1, epochs + 1):
+        # training batches
+        losses_train = train_utils.train_epoch(rgbd_model, train_dataset, epoch, optimizer, loss, train_metric, max_steps=None, logger=logger)
+
+        train_acc = np.float(train_metric.result()) # get train acc from training batches
+        train_metric.reset_states() # reset train acc for next epoch
+
+        # calculate validation metrics
+        losses_val = train_utils.validate_epoch(rgbd_model, test_dataset, epoch, loss, test_metric, max_steps=None, logger=logger)
+
+        val_acc = np.float(test_metric.result()) # get test acc from training batches
+        test_metric.reset_states() # reset test acc for next epoch
+
+        losses_train_mean = np.mean(losses_train)
+        losses_val_mean = np.mean(losses_val)
+
+        print(f'epoch: [{epoch}/{epochs}]; acc: {train_acc}; loss: {losses_train_mean}; val_acc: {val_acc}; val_loss: {losses_val_mean}')
+
+        # log to azure run
+        metric_logs = {'acc': train_acc, 'loss': losses_train_mean, 'val_acc': val_acc, 'val_loss': losses_val_mean}
+        for metric, value in metric_logs.items():
+            run.log(metric, value)
+
+        print('saving model...')
+        rgbd_model.save_weights(f'outputs/checkpoints/epoch{epoch}_weights.h5')
+
+
     print('done training!')
 
     # evaluate on test dataset
-    print('evaluating on test dataset...')
-    loss, acc = rgbd_model.evaluate(test_dataset)
-    run.log('test accuracy', np.float(acc))
-    run.log('test loss', np.float(loss))
+    # print('evaluating on test dataset...')
+    # loss, acc = rgbd_model.evaluate(test_dataset)
+    # run.log('test accuracy', np.float(acc))
+    # run.log('test loss', np.float(loss))
 
     print('saving model weights...')
-    rgbd_model.save_weights('outputs/rgbd_model0_weights.h5')
-    rgbd_model.save('outputs/rgbd_model0')
+    # rgbd_model.save_weights('outputs/rgbd_model_weights.h5')
+    rgbd_model.save('outputs/rgbd_model')
 
     print('All done!')
