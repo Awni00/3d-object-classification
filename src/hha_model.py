7a7275881b04a3881d63f22a3c05b58372a53a9c
Awni00 <awnyaltabaa@gmail.com>
2021-03-03 18:33:28 -0500

    fixed wrong variable names

--- src/hha_model.py
+++ src/hha_model.py
@@ -27,26 +27,26 @@ def build_model(input_shape, num_classes, model_name='hha_model'):
 
 
     hha_conv1 = Convolution2D(filters=32, kernel_size=(3,3), strides=(1, 1), padding='same', activation='relu', name='hha_conv1')(rescaled_hha)
-    hha_maxpool1 = MaxPooling2D(pool_size=(2, 2), strides=(2,2), name='hha_maxpool1')(depth_conv1)
+    hha_maxpool1 = MaxPooling2D(pool_size=(2, 2), strides=(2,2), name='hha_maxpool1')(hha_conv1)
 
-    hha_conv2 = Convolution2D(filters=64, kernel_size=(3,3), strides=(1, 1), padding='same', activation='relu', name='hha_conv2')(depth_maxpool1)
-    hha_maxpool2 = MaxPooling2D(pool_size=(2, 2), strides=(2,2), name='hha_maxpool2')(depth_conv2)
+    hha_conv2 = Convolution2D(filters=64, kernel_size=(3,3), strides=(1, 1), padding='same', activation='relu', name='hha_conv2')(hha_maxpool1)
+    hha_maxpool2 = MaxPooling2D(pool_size=(2, 2), strides=(2,2), name='hha_maxpool2')(hha_conv2)
 
-    hha_conv3 = Convolution2D(filters=64, kernel_size=(3,3), strides=(1, 1), padding='same', activation='relu', name='hha_conv3')(depth_maxpool2)
-    hha_maxpool3 = MaxPooling2D(pool_size=(2,2), strides=(2,2), name='hha_maxpool3')(depth_conv3)
+    hha_conv3 = Convolution2D(filters=64, kernel_size=(3,3), strides=(1, 1), padding='same', activation='relu', name='hha_conv3')(hha_maxpool2)
+    hha_maxpool3 = MaxPooling2D(pool_size=(2,2), strides=(2,2), name='hha_maxpool3')(hha_conv3)
 
-    hha_conv4 = Convolution2D(filters=128, kernel_size=(3,3), strides=(1, 1), padding='same', activation='relu', name='hha_conv4')(depth_maxpool3)
-    hha_maxpool4 = MaxPooling2D(pool_size=(2,2), strides=(2,2), name='hha_maxpool4')(depth_conv4)
+    hha_conv4 = Convolution2D(filters=128, kernel_size=(3,3), strides=(1, 1), padding='same', activation='relu', name='hha_conv4')(hha_maxpool3)
+    hha_maxpool4 = MaxPooling2D(pool_size=(2,2), strides=(2,2), name='hha_maxpool4')(hha_conv4)
 
-    hha_conv5 = Convolution2D(filters=128, kernel_size=(3,3), strides=(1, 1), padding='same', activation='relu', name='hha_conv5')(depth_maxpool4)
-    hha_maxpool5 = MaxPooling2D(pool_size=(2,2), strides=(2,2), name='hha_maxpool5')(depth_conv5)
+    hha_conv5 = Convolution2D(filters=128, kernel_size=(3,3), strides=(1, 1), padding='same', activation='relu', name='hha_conv5')(hha_maxpool4)
+    hha_maxpool5 = MaxPooling2D(pool_size=(2,2), strides=(2,2), name='hha_maxpool5')(hha_conv5)
 
-    hha_conv6 = Convolution2D(filters=64, kernel_size=(3,3), strides=(1, 1), padding='same', activation='relu', name='hha_conv6')(depth_maxpool5)
-    hha_maxpool6 = MaxPooling2D(pool_size=(2,2), strides=(2,2), name='hha_maxpool6')(depth_conv6)
+    hha_conv6 = Convolution2D(filters=64, kernel_size=(3,3), strides=(1, 1), padding='same', activation='relu', name='hha_conv6')(hha_maxpool5)
+    hha_maxpool6 = MaxPooling2D(pool_size=(2,2), strides=(2,2), name='hha_maxpool6')(hha_conv6)
 
-    hha_feat_vec = Flatten()(depth_maxpool6)
+    hha_feat_vec = Flatten()(hha_maxpool6)
 
-    dense1 = Dense(units=128, activation='relu', kernel_regularizer=reg, name='dense1')(depth_feat_vec)
+    dense1 = Dense(units=128, activation='relu', kernel_regularizer=reg, name='dense1')(hha_feat_vec)
     dense1_dropout = Dropout(0.5)(dense1)
     dense2 = Dense(units=64, activation='relu', kernel_regularizer=reg, name='dense2')(dense1_dropout)
     dense2_dropout = Dropout(0.5)(dense2)
